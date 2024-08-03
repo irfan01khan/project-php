396491b5c145ea4c2d7c95d1174edd886faa9258
irfan khan <136296523+irfan01khan@users.noreply.github.com>
2024-08-03 18:33:18 +0530

    Create db.inc.php

---
+++ db.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_SERVER', 'php-project-rds.cv8wa6e8q5jo.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', '12345678-asdF');
+define('DB_DATABASE', 'php');
+?>
